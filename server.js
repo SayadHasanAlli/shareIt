736f1d1702d72401195cd3a8a2a33d250899f32f
Hasan <hasansd432@gmail.com>
2023-08-26 18:44:00 +0530

    initial

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const app=express();
+const path=require('path')
+const PORT=process.env.PORT || 5000;
+
+app.use(express.static('public'))
+app.use(express.json());
+
+const connectDB = require('./config/db');
+connectDB();//importing the database
+
+//Template Engine
+app.set('views',path.join(__dirname,'/views'));//all html templates are in views
+app.set('view engine','ejs')
+
+//Routes
+app.use('/api/files', require('./routes/files')) // take file from files.js - uoload and send it to end point of /api/files
+app.use('/files', require('./routes/show'))
+app.use('/files/download', require('./routes/download'))
+
+
+app.listen(PORT, ()=>{
+    console.log(`LIstening on port ${PORT}`);
+})
+
+// to run the server enter yarn dev on terminal
