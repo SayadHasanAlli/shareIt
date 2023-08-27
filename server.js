c522417fca94c8c43ee18f02ee71d0267bc65ee4
Hasan <hasansd432@gmail.com>
2023-08-27 16:16:20 +0530

    add script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const app=express();
 const path=require('path')
+const cors= require('cors')
 const PORT=process.env.PORT || 5000;
 
 app.use(express.static('public'))
@@ -8,6 +9,12 @@ app.use(express.json());
 
 const connectDB = require('./config/db');
 connectDB();//importing the database
+
+//cors
+const corsOptions={
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions))
 
 //Template Engine
 app.set('views',path.join(__dirname,'/views'));//all html templates are in views
