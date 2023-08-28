c39d41c0003bd8d3c06555c029d179d2bfa2c3ea
Hasan <hasansd432@gmail.com>
2023-08-28 17:17:12 +0530

    updated

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -148,7 +148,7 @@ module.exports = ({emailFrom, downloadLink, size, expires}) => {
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
                                 <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">ShareIt ---------------------------</span>
-                                <br> Want to share a file? <a href="https://shareit-8wyl.onrender.com" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">ShareIt</a>.
+                                <br> Want to share a file? <a href="https://shareitfileshare.netlify.app/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">ShareIt</a>.
                             </td>
                             </tr>
                             <tr>
